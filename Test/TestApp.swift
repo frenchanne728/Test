2106300143e3943d2840cb64ff8691bf5879837a
Anne French <frenchanne728@gmail.com>
2020-10-21 09:14:01 -0500

    Initial Commit

---
+++ Test/TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestApp.swift
+//  Test
+//
+//  Created by Anne French on 10/21/20.
+//
+
+import SwiftUI
+
+@main
+struct TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
